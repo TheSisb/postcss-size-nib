79fa85d31d2cdc05918e27fc24d87e6460a9d037
TheSisb <shadiisber@gmail.com>
2017-07-03 13:33:22 -0700

    update postcss to 6 and add tests

--- index.test.js
+++ index.test.js
@@ -1,5 +1,4 @@
 var postcss = require('postcss');
-
 var plugin = require('./');
 
 function run(input, output, opts) {
@@ -10,10 +9,10 @@ function run(input, output, opts) {
         });
 }
 
-/* Write tests here
-
-it('does something', () => {
-    return run('a{ }', 'a{ }', { });
+it('convert size to width and height', () => {
+    return run('a{size: 10px;}', 'a{width: 10px;height: 10px;}', { });
 });
 
-*/
+it('convert two size values to width and height', () => {
+    return run('a{size: 10px 15px;}', 'a{width: 10px;height: 15px;}', { });
+});
